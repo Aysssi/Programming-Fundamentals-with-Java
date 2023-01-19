17b67b81e048ca1b54e725ccfc035e256cef7a87
Aysssi <ayselchausheva@abv.bg>
2023-01-19 09:04:35 +0200

    Update add boolean variable

--- Data-Types-and-Variables-Lab/P10SpecialNumbers.java
+++ Data-Types-and-Variables-Lab/P10SpecialNumbers.java
@@ -8,22 +8,26 @@ public class P10SpecialNumbers {
 
         int num = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 1; i <= num ; i++) {
-
+        for (int i = 1; i <= num; i++) {
             int currentNum = i;
-            int sum = 0;
 
-            while (currentNum > 0){
-                sum = sum + currentNum % 10 ;
-                currentNum = currentNum /10;
+            int sum = 0;
+            while (currentNum > 0) {
+                sum = sum + (currentNum % 10);
+                currentNum = currentNum / 10;
+            }
+
+            boolean isSpecial = sum == 5 || sum == 7 || sum == 11;
+
+            if (isSpecial) {
+                System.out.printf("%d -> True%n", i);
+            } else {
+                System.out.printf("%d -> False%n", i);
 
             }
-          if (sum == 5 || sum == 7 || sum == 11){
-              System.out.printf("%d -> True%n",i);
-          }else {
-              System.out.printf("%d -> False%n",i);
-          }
         }
+
+
 
 
         }
